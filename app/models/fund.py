9cc6c3a80d8a1f70f8f6273aaa80cea9e1ef4639
taoting <335354289@qq.com>
2019-08-03 21:52:25 +0800

    修复bug

--- app/models/fund.py
+++ app/models/fund.py
@@ -20,6 +20,8 @@ def apply(user_id, name, money, remark):
         r.name = name
         r.money = money
         r.remark = remark
+        r.apply_time = datetime.datetime.now()
+        r.status = 0
         db.session.add(r)
 
 
@@ -31,9 +33,9 @@ def get_apply_list_by_user_id(user_id):
         'remark': i.remark,
         'apply_user_id': i.apply_user_id,
         'apply_user_nickname': i.apply_user.nickname,
-        'apply_time': i.apply_user_time,
+        'apply_time': i.apply_time,
         'approval_user_id': i.approval_user_id,
-        'approval_user_nickname': i.approval_user.nickname,
+        'approval_user_nickname': i.approval_user.nickname if i.approval_user else None,
         'approval_time': i.approval_time,
         'status': i.status
-    } for i in Application.query.filter_by(user_id=user_id).all()]
+    } for i in Application.query.filter_by(apply_user_id=user_id).all()]
